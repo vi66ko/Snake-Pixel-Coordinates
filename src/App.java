536a7b085dfa9c53048861e92c415871ff40e0f3
V.Stamenov <vi66ko@mail.bg>
2025-01-02 17:41:31 +0000

    java + javaFx setup

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+
+
+public class App  {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
