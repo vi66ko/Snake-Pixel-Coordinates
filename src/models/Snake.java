0ecebd4910fc393542d805cc8caa8d67685630c0
V.Stamenov <vi66ko@mail.bg>
2025-01-07 23:23:48 +0000

    adding move method so the snake can handle own moves

--- src/models/Snake.java
+++ src/models/Snake.java
@@ -5,7 +5,12 @@ import javafx.scene.paint.Color;
 public class Snake extends GameObject{
 
     public Snake( int size) {
-        super(25, Color.rgb(146, 255, 127));
+        super(size, Color.rgb(146, 255, 127));
     }
-
+    public void move(PlayableArea playableArea) {
+        this.setPos( this.getPosX() + this.getVelocityX(), this.getPosY() + this.getVelocityY());
+        int x = (this.getPosX() + playableArea.getWidth() / this.getSize()) % (playableArea.getWidth() / this.getSize());
+        int y = (this.getPosY() + playableArea.getHeight() / this.getSize()) % (playableArea.getHeight() / this.getSize());
+        this.setPos(x, y);
+    }
 }
