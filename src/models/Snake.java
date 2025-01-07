034b06ead32dd2d58067b045721ed61a66d48fdf
V.Stamenov <vi66ko@mail.bg>
2025-01-07 16:29:21 +0000

    adding snake object

---
+++ src/models/Snake.java
@@ -0,0 +1,11 @@
+package models;
+
+import javafx.scene.paint.Color;
+
+public class Snake extends GameObject{
+
+    public Snake( int size) {
+        super(25, Color.rgb(146, 255, 127));
+    }
+
+}
