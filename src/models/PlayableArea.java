efca16f27d0f090e3ea7b7b6d8fbb52105551f6b
V.Stamenov <vi66ko@mail.bg>
2025-01-07 17:06:47 +0000

    adding option for position of the playable area

--- src/models/PlayableArea.java
+++ src/models/PlayableArea.java
@@ -3,10 +3,12 @@ package models;
 import javafx.scene.paint.Color;
 
 public class PlayableArea {
+    private int posX = 0;
+    private int posY = 0;
     private int width;
     private int height;
     private Color background = Color.rgb(91, 98, 95);
-
+    
     public PlayableArea(int width, int height) {
         this.width = width;
         this.height = height;
@@ -17,6 +19,13 @@ public class PlayableArea {
         this.height = height;
     }
 
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
     public int getWidth() {
         return width;
     }
