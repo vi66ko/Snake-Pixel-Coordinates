9249c3394c8343be55d16c0fdbbb785cb1576f56
V.Stamenov <vi66ko@mail.bg>
2025-01-08 12:49:32 +0000

    adding legend with coordinates on the grid

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -35,26 +35,28 @@ public class GUI implements EventHandler<KeyEvent> {
     private PlayableArea playableArea;
     private Label scores;
     private Label coordinates;
+    private int canvasOffset;
 
-
-    public GUI(Stage primaryStage, PlayableArea playableArea, Controller controller ){
+    public GUI(Stage primaryStage, PlayableArea playableArea, Controller controller, int canvasOffset){
         Debug.trace("GUI::<constructor>");
         this.controller = controller;
         this.primaryStage = primaryStage;
         this.playableArea = playableArea;
+        this.canvasOffset = canvasOffset;
         this.pane = new Pane();
         this.pane.setId("snake-pane");
         
         
         this.canvas = new Canvas(playableArea.getWidth(), playableArea.getHeight());
-        this.canvas.setLayoutY(50);
+        this.canvas.setLayoutY(canvasOffset);
+        this.canvas.setLayoutX(50);
         this.graphicsContext = canvas.getGraphicsContext2D();
 
         this.paintCanvas();
 
         this.pane.getChildren().add(canvas);
 
-        this.scene = new Scene(pane, playableArea.getWidth(), playableArea.getHeight() + 100);
+        this.scene = new Scene(pane, playableArea.getWidth() + 100, playableArea.getHeight() + 200);
         this.scene.getStylesheets().add("./style.css");
         this.scene.setOnKeyPressed(this);
 
@@ -74,15 +76,36 @@ public class GUI implements EventHandler<KeyEvent> {
         Group grid = new Group();
         
         for (int i = 0; i <= this.playableArea.getWidth() / size; i++) {
-            Line horizontal = new Line(0, i * size + 50, this.playableArea.getWidth(), i * size + 50);
-            Line vertical = new Line(i * size, 50, i * size, this.playableArea.getHeight() + 50);
+            Label horizontalLegend = new Label(String.valueOf(i));
+            horizontalLegend.setId("grid-legend");
+            Label verticalLegend = new Label(String.valueOf(i));
+            verticalLegend.setId("grid-legend");
+
+            Debug.trace("" +((i == 0) ? size / 2 : size * i + size / 2));
+
+            Line horizontal = new Line(50, i * size + this.canvasOffset, this.playableArea.getWidth() +50, i * size + this.canvasOffset);
+            Line vertical = new Line(i * size + 50 , this.canvasOffset, i * size + 50, this.playableArea.getHeight() + this.canvasOffset);
             
             horizontal.setStroke(Color.rgb(150, 150, 150,  0.7));
             vertical.setStroke(Color.rgb(150, 150, 150,  0.7));
             grid.getChildren().add(horizontal);
             grid.getChildren().add(vertical);
+
+            if( i < this.playableArea.getHeight() / size) {
+                horizontalLegend.setTranslateX( size * i + 50 + size / 2);
+                horizontalLegend.setTranslateY(40);
+    
+                verticalLegend.setTranslateX(20);
+                verticalLegend.setTranslateY( size * i + 70 + size / 2);
+                //horizontalLegend.setTranslateX(50 + ((i == 0) ? size / 2 : size * i));
+                //verticalLegend.setTranslateY((i == 0) ? size / 2 : size * i); WHY DOES NOT WORK ???           grid.getChildren().add(horizontalLegend);
+
+                grid.getChildren().add(horizontalLegend);
+                grid.getChildren().add(verticalLegend);
+            }
         }
         this.pane.getChildren().add(grid);
+        
     }
     public void drawScores(int value){
         this.scores = new Label("Score: " + value);
@@ -101,6 +124,10 @@ public class GUI implements EventHandler<KeyEvent> {
 
     public void drawSnake(Snake snake){
         this.drawRectangularGameObject(snake);
+        
+        for (GameObject gameObject : snake.getBody()) {
+            this.drawRectangularGameObject(gameObject);
+        }
     }
     public void drawFood(Food food){
         this.drawRectangularGameObject(food);
@@ -116,12 +143,11 @@ public class GUI implements EventHandler<KeyEvent> {
         this.coordinates.setId("coordinates");
         this.coordinates.setTranslateX(100);
         Debug.trace("PlayableArea: " + playableArea.getHeight());
-        this.coordinates.setTranslateY(50 + this.playableArea.getHeight() + 10);
+        this.coordinates.setTranslateY(this.canvasOffset + this.playableArea.getHeight() + 10);
         this.pane.getChildren().add(this.coordinates);
     }
     public void updateCoordinates(GameObject gameObject){
         this.coordinates.setText("Coordinates:                 X = " + gameObject.getPosX() + "       Y = "+ gameObject.getPosY() );
-;
     }
 
 }
